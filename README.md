5762b80de976075eff43749d80061cc270cf8f7c
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-06-03 16:57:48 +0700

    Toán tử In JavaScript

---
+++ README.md
@@ -0,0 +1,7 @@
+*Giới thiệu 1 số hàm built-in
+1. Alert
+2. Console
+3. Confirm
+4. Prompt
+5. Set timeout
+6. Set Interval
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('Xin Chào Huy Nguyễn');
+// setTimeout(function() {
+//     alert('Xin Chào Huy Nguyễn');
+// }, 3000)
+
+
+// setInterval(function(){
+//     console.log("JavaScript" + Math.random())
+// }, 2000)
+
+
+var a = 1;
+var b = 2;
+
+if (a==b) {
+    console.log('Toán tử đúng')  ;  
+}
+else{
+    console.log('Toán tử sai');
+}
