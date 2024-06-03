59473bd1b6d54a00cd2e041791bfe382b40dac17
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-06-03 23:17:55 +0700

    update Lộ Trình học web

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-*Giới thiệu 1 số hàm built-in
+- Giới thiệu 1 số hàm built-in
+
 1. Alert
 2. Console
 3. Confirm
 4. Prompt
 5. Set timeout
 6. Set Interval
+
+- 6 Toán tử khi để vào để so sánh điều kiên nó sẽ thành False
+
+1. 0
+2. false
+3. '' , ""
+4. undefined
+5. NaN
+6. Null
+
+Bài 17: Kiểu dữ liệu trong Java Script
+
+1. Dữ liệu nguyên thủy - Primitive Data
+
+- Number
+- String
+- Boolean
+- Undefined
+- Null
+- Symbol
+
+2. Dữ liệu phức tạp - Complex Data
+
+- Function
+- Object
--- main.js
+++ main.js
@@ -8,12 +8,32 @@
 // }, 2000)
 
 
-var a = 1;
-var b = 2;
+// var a = 1;
+// var b = 2;
 
-if (a==b) {
-    console.log('Toán tử đúng')  ;  
-}
-else{
-    console.log('Toán tử sai');
-}
+// if (a==b) {
+//     console.log('Toán tử đúng')  ;  
+// }
+// else{
+//     console.log('Toán tử sai');
+// }
+
+
+// var age = 25
+// // Object
+// var myObject = {
+//     age : 25,
+//     name : 'Nguyễn Thanh Huy',
+//     adress: 'HCM City',
+//     myFunction : function(){
+
+//     }
+// }
+
+// console.log(typeof age)
+
+// Templete String
+var firstName = 'Nguyen Thanh'
+var lastName = 'Huy'
+
+console.log(`My Name is: ${firstName} ${lastName}`)
